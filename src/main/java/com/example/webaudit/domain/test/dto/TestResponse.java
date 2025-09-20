350b65a51bd20b8ea108856b671d5310dbd7508c
Eonseo <eonseo15@daum.net>
2025-09-20 22:45:30 +0900

    [Feature] TestResponse.java (DTO) 추가

---
+++ src/main/java/com/example/webaudit/domain/test/dto/TestResponse.java
@@ -0,0 +1,21 @@
+package com.example.webaudit.domain.test.dto;
+
+import com.example.webaudit.domain.test.entity.TestEntity;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TestResponse {
+    private String id;
+    private String url;
+    private String status;
+
+    public static TestResponse from(TestEntity entity) {
+        return TestResponse.builder()
+                .id(entity.getId())
+                .url(entity.getUrl())
+                .status(entity.getStatus().name())
+                .build();
+    }
+}
